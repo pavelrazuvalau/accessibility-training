e6d65446f035f633169fdad474d7ac337566f0b8
Pavel Razuvalau <pavel@razuvalov.com>
2020-01-13 03:20:32 +0300

    feat: implement keyboard navigation to the submenu

--- js/custom.js
+++ js/custom.js
@@ -31,6 +31,16 @@ function initLiveCounter() {
   setInterval(function() { liveCounter.textContent = "Current count: " + currentCount++ }, 1000 * 60);
 }
 
+function openSubMenu(trigger, container) {
+  container.classList.add('is-active');
+  trigger.setAttribute('aria-expanded', "true");
+}
+
+function closeSubMenu(trigger, container) {
+  container.classList.remove('is-active');
+  trigger.setAttribute('aria-expanded', "false");
+}
+
 (function() {
   var burger = document.querySelector(".burger");
   var menu = document.querySelector("#" + burger.dataset.target);
@@ -48,7 +58,7 @@ function initLiveCounter() {
 })();
 
 var topicsTabList = document.querySelector('#list-of-topics-tabs');
-var topicTabs = [...topicsTabList.querySelectorAll("li")];
+var topicTabs = Array.from(topicsTabList.querySelectorAll("li"));
 
 topicsTabList.addEventListener("keydown", function(event) {
   var currentTab = topicsTabList.querySelector("li.is-active");
@@ -107,4 +117,87 @@ topicTabs.forEach(function(navEl) {
       toggleTab(event.target.id, event.target.dataset.target)
     }
   };
+});
+
+var subMenu = document.querySelector('#submenu');
+var subMenuTrigger = document.querySelector('#submenu-button');
+var subMenuContainer = document.querySelector('#sumbenu-container');
+
+var subMenuItems = Array.from(subMenuContainer.querySelectorAll('li')).filter(function(item) { return item.getAttribute("role") !== "separator" });
+
+subMenuTrigger.addEventListener('click', function() {
+  if (!subMenuContainer.classList.contains('is-active')) {
+    openSubMenu(subMenuTrigger, subMenuContainer);
+    subMenuItems[0].firstElementChild.focus();
+  }
+});
+
+subMenu.addEventListener('mouseenter', function() {
+  openSubMenu(subMenuTrigger, subMenuContainer);
+});
+
+subMenu.addEventListener('mouseleave', function() {
+  closeSubMenu(subMenuTrigger, subMenuContainer);
+});
+
+document.addEventListener('click', function(event) {
+  if (!subMenu.contains(event.target)) {
+    closeSubMenu(subMenuTrigger, subMenuContainer);
+  }
+});
+
+subMenuContainer.addEventListener('keydown', function(event) {
+  var possiblyAnotherActiveItem = subMenuItems.find(function(item) { return item.contains(document.activeElement) });
+  var currentItem = possiblyAnotherActiveItem || subMenuItems[0];
+  var currentItemIdx = subMenuItems.indexOf(currentItem);
+
+  switch(event.keyCode) {
+    case 38: { // Arrow up
+      if (subMenuItems[currentItemIdx - 1]) {
+        subMenuItems[currentItemIdx - 1].firstElementChild.focus();
+      } else {
+        subMenuItems[subMenuItems.length - 1].firstElementChild.focus();
+      }
+      event.preventDefault();
+      break;
+    }
+    case 40: { // Arrow down
+      if (subMenuItems[currentItemIdx + 1]) {
+        subMenuItems[currentItemIdx + 1].firstElementChild.focus();
+      } else {
+        subMenuItems[0].firstElementChild.focus();
+      }
+      event.preventDefault();
+      break;
+    }
+    case 32: // Space
+    case 13: { // Enter
+      document.activeElement.click();
+      event.preventDefault();
+      break;
+    }
+    case 36: { // Home
+      subMenuItems[0].firstElementChild.focus();
+      event.preventDefault();
+      break;
+    }
+    case 35: { // End
+      subMenuItems[subMenuItems.length - 1].firstElementChild.focus();
+      event.preventDefault();
+      break;
+    }
+    case 27: { // Escape
+      closeSubMenu(subMenuTrigger, subMenuContainer);
+      subMenuTrigger.focus();
+      event.preventDefault();
+      break;
+    }
+    case 9: { // tab
+      closeSubMenu(subMenuTrigger, subMenuContainer);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
 });
